0efe03511964088e2ba33261e5e5a4216fecd3f5
Yan Takhtaulov <kwockeeez@gmail.com>
2022-09-27 22:24:06 +0300

    add sample_sdl v2

--- main.c
+++ main.c
@@ -1,13 +1,28 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_timer.h>
+#include <SDL2/SDL_mixer.h>
+
+#include <enet/enet.h>
 
 #include <stdbool.h>
 #include <stdio.h>
 
+#define WINDOW_WIDTH    1280
+#define WINDOW_HEIGHT    720
+
+#define RECT_WIDTH_SIZE    5 
+#define RECT_HEIGHT_SIZE   5 
+#define RECT_SPEED       400
+
+#define FREQUENCY      44100
+#define CHUNKSIZE      2048
+#define LOOPS          5
+
 int main(int argc, char *argv[])
 {
-    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) != 0) {
+    Uint32 sdl_flags = SDL_INIT_VIDEO | SDL_INIT_EVENTS;
+    if (SDL_Init(sdl_flags) != 0) {
         fprintf(stderr, "Initializing SDL error: %s\n", SDL_GetError());
         return EXIT_FAILURE;
     }
@@ -18,28 +33,43 @@ int main(int argc, char *argv[])
         return EXIT_FAILURE;
     }
 
-    SDL_Window *window;
-    SDL_Renderer *renderer;
-    SDL_Surface *surface;
-    SDL_Texture *texture;
+    SDL_Window *window =     NULL;
+    SDL_Renderer *renderer = NULL;
+    SDL_Surface *surface =   NULL;
+    SDL_Texture *texture =   NULL;
+    Mix_Music *music =       NULL;
     SDL_Event event;
     SDL_Rect rect;
-
-    int speed = 300;
-    bool is_close = false;
 
     window = SDL_CreateWindow(
         "Game",
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
-        1000,
-        1000,
+        WINDOW_WIDTH,
+        WINDOW_HEIGHT,
         SDL_WINDOW_OPENGL
     );
     if (window == NULL) {
         fprintf(stderr, "Could not create window: %s\n", SDL_GetError());
         return EXIT_FAILURE;
     }
+
+    SDL_Delay(100);
+
+    Mix_OpenAudio(
+        FREQUENCY,
+        MIX_DEFAULT_FORMAT,
+        MIX_DEFAULT_CHANNELS,
+        CHUNKSIZE
+    );
+
+    music = Mix_LoadMUS("music/anime.mp3");
+    if (music == NULL) {
+        fprintf(stderr, "%s\n", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+
+    Mix_PlayMusic(music, LOOPS);
 
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
@@ -55,12 +85,13 @@ int main(int argc, char *argv[])
 
     SDL_QueryTexture(texture, NULL, NULL, &rect.w, &rect.h);
 
-    rect.w /= 6;
-    rect.h /= 6;
+    rect.w /= RECT_WIDTH_SIZE;
+    rect.h /= RECT_HEIGHT_SIZE;
 
-    rect.x = (1000 - rect.w) / 2;
-    rect.y = (1000 - rect.h) / 2;
+    rect.x = (WINDOW_WIDTH - rect.w) / 2;
+    rect.y = (WINDOW_HEIGHT - rect.h) / 2;
 
+    bool is_close = false;
     while (!is_close) {
         while (SDL_PollEvent(&event)) {
             switch (event.type) {
@@ -72,22 +103,22 @@ int main(int argc, char *argv[])
                     switch (event.key.keysym.scancode) {
                         case SDL_SCANCODE_W:
                         case SDL_SCANCODE_UP:
-                            rect.y -= speed / 30;
+                            rect.y -= RECT_SPEED / 30;
                             break;
 
                         case SDL_SCANCODE_A:
                         case SDL_SCANCODE_LEFT:
-                            rect.x -= speed / 30;
+                            rect.x -= RECT_SPEED / 30;
                             break;
 
                         case SDL_SCANCODE_S:
                         case SDL_SCANCODE_DOWN:
-                            rect.y += speed / 30;
+                            rect.y += RECT_SPEED / 30;
                             break;
 
                         case SDL_SCANCODE_D:
                         case SDL_SCANCODE_RIGHT:
-                            rect.x += speed / 30;
+                            rect.x += RECT_SPEED / 30;
                             break;
 
                         default:
@@ -97,16 +128,16 @@ int main(int argc, char *argv[])
             }
         }
 
-        if (rect.x + rect.w > 1000) {
-            rect.x = 1000 - rect.w;
+        if (rect.x + rect.w > WINDOW_WIDTH) {
+            rect.x = WINDOW_WIDTH - rect.w;
         }
 
         if (rect.x < 0) {
             rect.x = 0;
         }
 
-        if (rect.y + rect.h > 1000) {
-            rect.y = 1000 - rect.h;
+        if (rect.y + rect.h > WINDOW_HEIGHT) {
+            rect.y = WINDOW_HEIGHT - rect.h;
         }
 
         if (rect.y < 0) {
@@ -118,7 +149,8 @@ int main(int argc, char *argv[])
 
         SDL_RenderPresent(renderer);
 
-        SDL_Delay(1000 / 60);
+        Uint32 middle = (WINDOW_WIDTH + WINDOW_HEIGHT) / 2;
+        SDL_Delay(middle / 60);
     }
 
     SDL_DestroyTexture(texture);
